5afa2c327343418045cf20d46aa6f1261907ccea
Kuba <jakub.sztajerowski123@gmail.com>
2021-10-12 13:50:34 +0200

    tni-24

--- src/main/java/pl/carwebapp/controllers/MessageController.java
+++ src/main/java/pl/carwebapp/controllers/MessageController.java
@@ -3,8 +3,10 @@ package pl.carwebapp.controllers;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import pl.carwebapp.model.ChatMessage;
 
+@CrossOrigin
 @Controller
 public class MessageController {
 
